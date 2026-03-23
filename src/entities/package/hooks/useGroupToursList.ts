eec7e4b2b78270f58ec525c13451a029b0f1d572
Vahe Minasyan <vahe.minasyan.2000.at@gmail.com>
2026-03-23 12:03:29 +0400

    Changed group tour list get logic

--- src/entities/package/hooks/useGroupToursList.ts
+++ src/entities/package/hooks/useGroupToursList.ts
@@ -1,19 +1,34 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query'
+import { useInfiniteQuery, UseInfiniteQueryOptions } from '@tanstack/react-query'
 import { packageUseCases, type GroupTourList } from '@entities/package'
 import { PACKAGE_REQUEST_REFETCH_INTERVAL } from '@shared/configs'
 
 export const useGroupToursList = (
-	params?: { page?: number | null; limit?: number | null },
-	options?: Omit<UseQueryOptions<GroupTourList>, 'queryFn' | 'queryKey'>
+	params?: { limit?: number | null },
+	options?: Omit<
+    UseInfiniteQueryOptions<GroupTourList>,
+    'queryFn' | 'queryKey' | 'initialPageParam' | 'getNextPageParam'
+  >
 ) => {
-	return useQuery({
-	  ...(options || {}),
-		refetchInterval: false,
-		refetchOnMount: false,
-		refetchOnWindowFocus: false,
-		refetchOnReconnect: false,
-		staleTime: PACKAGE_REQUEST_REFETCH_INTERVAL,
-		queryFn: () => packageUseCases.getGroupTours(params),
-		queryKey: ['group-tours', params?.page ?? null, params?.limit ?? null],
-	})
+	const limit = params?.limit ?? 8
+
+  return useInfiniteQuery({
+    ...(options || {}),
+    refetchInterval: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    staleTime: PACKAGE_REQUEST_REFETCH_INTERVAL,
+    queryKey: ['group-tours', limit],
+    initialPageParam: 1,
+    queryFn: ({ pageParam }) =>
+      packageUseCases.getGroupTours({
+        page: pageParam as number,
+        limit,
+      }),
+    getNextPageParam: (lastPage) => {
+      const currentPage = lastPage?.pagination?.page ?? 1
+      const totalPages = lastPage?.pagination?.totalPages ?? 1
+      return currentPage < totalPages ? currentPage + 1 : undefined
+    },
+  })
 }
